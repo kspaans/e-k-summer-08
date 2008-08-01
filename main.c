cd685f4630dc1559421cbb97b0adf23d79a514a1
Kyle Spaans <kspaans@student.math.uwaterloo.ca>
2008-07-31 22:26:57 -0400

    Life:  added a license, the Artistic License 2.0

--- main.c
+++ main.c
@@ -2,6 +2,17 @@
  * elouie@student.cs.uwaterloo.ca, kspaans@student.math.uwaterloo.ca
  *
  *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the Artistic License 2.0 as published by
+ *   the Perl Foundation. You may obtain a copy of the license at
+ *
+ *   http://www.perlfoundation.org/artistic_license_2_0
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   Artistic License 2.0 for more details.
+ *
  */
 /* Erik & Kyle Summer 2008
  * Conway's Game of Life
